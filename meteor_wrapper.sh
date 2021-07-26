ce3019b5ec2c4adc19cd370431b5373679d597e2
theoportlock <zn.tportlock@gmail.com>
2021-07-26 15:37:44 +0100

    Fixed rosalind version

--- meteor_wrapper.sh
+++ meteor_wrapper.sh
@@ -1,10 +1,9 @@
-#!/bin/bash -l
-#SBATCH --account=snic2021-5-248
-#SBATCH --partition=core
+#!/bin/bash -le
+#SBATCH --job-name=meteor
 #SBATCH --ntasks=10
-#SBATCH --time=2-06:00:00
-#SBATCH --job-name=METEOR
-##SBATCH --output=%j 
+#SBATCH --partition=shared
+#SBATCH --mem=20G
+#SBATCH --time=1-00:00:00
 
 function Run {
 	echo "...$(date): $1 - begin"; echo ""
@@ -13,19 +12,20 @@ function Run {
 	|| (echo "...$(date): $1 - failed"; return 1)
 }
 function Parse_variables {
-	sampleId=$(basename $sample .fastq.gz)
-	v_project_dir="$TMPDIR"
+	v_sampleId=$(basename $sample)
+	v_project_dir="/scratch/users/k1809704/$v_sampleId"
+	v_ini_file=$v_project_dir/tmpini.ini
+	sed "s|meteor.reference.dir=.*|meteor.reference.dir=$v_project_dir/reference|g" $ini_file > $v_inifile
+	source $v_ini_file > /dev/null 2>&1
+
 	v_prefix=$( grep "meteor.counting.prefix.name" $ini_file | sed "s/^.*=//g" )
 	v_workdir="${v_project_dir}/working"
 	v_refdir="${v_project_dir}/reference"
 	v_fastqgz1="$(readlink -f $seq_data_dir/${sample}${forward_identifier})"
 	v_fastqgz2="$(readlink -f $seq_data_dir/${sample}${reverse_identifier})"
-	v_fastqgz1_unzip="${v_workdir}/$(basename $v_fastqgz1 .gz).unzipped"
-	v_fastqgz2_unzip="${v_workdir}/$(basename $v_fastqgz2 .gz).unzipped"
-	v_trimmedS="${v_workdir}/${sampleId}.trimmed.s.fastq"
-	v_final1="${v_workdir}/${sampleId}_1.fastq"
-	v_final2="${v_workdir}/${sampleId}_2.fastq"
-	v_sampledir="${v_project_dir}/${catalog_type}/sample/${sampleId}"
+	v_final1="${v_workdir}/${v_sampleId}.1.fastq"
+	v_final2="${v_workdir}/${v_sampleId}.2.fastq"
+	v_sampledir="${v_project_dir}/${catalog_type}/sample/${v_sampleId}"
 	vars="$(compgen -A variable | grep "^v_.*")"
 	for var in ${vars}; do echo "${var}=${!var}"; done
 	for var in ${vars}; do if [[ -z "${!var}" ]]; then echo "missing argument ${var}" ; return 1 ; fi ; done
@@ -39,21 +39,16 @@ function Send {
 	cp $v_fastqgz1 $v_workdir
 	cp $v_fastqgz2 $v_workdir
 	cp -r ${reference_dir}/* $v_refdir
-	cp -r $trimFasta $v_project_dir
-}
-function Decompress {
-	zcat ${v_workdir}/$(basename $v_fastqgz1) > $v_fastqgz1_unzip & pid1=$!
-	zcat ${v_workdir}/$(basename $v_fastqgz2) > $v_fastqgz2_unzip & pid2=$!
-	trap "kill -2 $pid1 $pid2" SIGINT
-	wait
+	cp $trimFasta $v_project_dir
 }
 function Trim {
-	[[ -f $v_fastqgz1_unzip && -f $v_fastqgz2_unzip ]] \
-	|| (echo "zip file not found" ; return 1 )
-	java -jar ${ALIEN_TRIMMER}/AlienTrimmer.jar \
+	echo $v_project_dir/$(basename $trimFasta)
+	java -jar ${ALIEN_TRIMMER} \
 		-k 10 -l 45 -m 5 -p 40 -q 20 \
-		-if ${v_fastqgz1_unzip} -ir ${v_fastqgz2_unzip} -c $TMPDIR/$(basename $trimFasta) \
-		-of ${v_final1} -or ${v_final2} -os ${v_trimmedS} \
+		-1 $v_workdir/$(basename ${v_fastqgz1}) \
+		-2 $v_workdir/$(basename ${v_fastqgz2}) \
+		-a $v_project_dir/$(basename $trimFasta) \
+		-o $v_workdir/${v_sampleId} \
 	|| return 1
 }
 function Import {
@@ -65,12 +60,13 @@ function Import {
 		-i ${v_project_dir}/${catalog_type}/sample \
 		-p ${catalog_type} \
 		-t ${seq_platform} \
-		-m "${sampleId}*" \
+		-m "${v_sampleId}*" \
 	|| return 1
 }
 function Map_reads {
+	readlink -f $v_ini_file
 	ruby ${meteor_rep}/meteor-pipeline/meteor.rb \
-		-w ${ini_file} \
+		-w ${v_ini_file} \
 		-i ${v_sampledir} \
 		-p ${v_project_dir}/${catalog_type} \
 		-o mapping \
@@ -81,16 +77,16 @@ function Quantify {
 		-p ${v_project_dir}/${catalog_type} \
 		-f ${v_project_dir}/${catalog_type}/profiles \
 		-t smart_shared_reads \
-		-w ${ini_file} \
-		-o ${sampleId} ${v_project_dir}/${catalog_type}/mapping/${sampleId}/${sampleId}_${v_prefix}_gene_profile/census.dat \
-	&& rm -r ${v_project_dir}/${catalog_type}/mapping/${sampleId} \
+		-w ${v_ini_file} \
+		-o ${v_sampleId} ${v_project_dir}/${catalog_type}/mapping/${v_sampleId}/${v_sampleId}_${v_prefix}_gene_profile/census.dat \
+	&& rm -r ${v_project_dir}/${catalog_type}/mapping/${v_sampleId} \
 	|| return 1
 }
 function Recover {
-	rm $TMPDIR/$(basename $trimFasta)
+	rm $v_project_dir/$(basename $trimFasta)
 	rm -r ${v_workdir}
 	rm -r ${v_refdir}
-	rm $ini_file
+	rm $v_ini_file
 	rsync -aurvP --remove-source-files $v_project_dir/ $project_dir_rel
 }
 
@@ -98,7 +94,6 @@ Main() {
 	Run Parse_variables &&
 	Run Init &&
 	Run Send &&
-	Run Decompress &&
 	Run Trim &&
 	Run Import &&
 	Run Map_reads &&
@@ -106,26 +101,23 @@ Main() {
 	Run Recover
 }
 
-echo "...$(date): - begin running script"
-cat $0
-echo ""
-
 # Load modules
 echo "...$(date): - load modules"
-module load bioinfo-tools
-module load ruby/2.6.2
-module load AlienTrimmer/0.4.0
-module load bowtie2/2.3.4.1
+module load apps/bowtie2
+module load devtools/ruby
+module load apps/openjdk
 echo "...$(date): - done"; echo "" 
 
 # Load ini file
 echo "...$(date): - load ini file"
-echo $TMPDIR/$(basename $1)
-sed "s|meteor.reference.dir=.*|meteor.reference.dir=$TMPDIR/reference|g" $1 > $TMPDIR/$(basename $1)
-ini_file=$TMPDIR/$(basename $1)
+ini_file=$1
 source $ini_file > /dev/null 2>&1
 cat $ini_file
 echo "...$(date): - done"; echo "" 
+
+echo "...$(date): - begin running script"
+cat $0
+echo ""
 
 # Run
 Main

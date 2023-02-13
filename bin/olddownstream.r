07d2b5038612c83143e72ff7471bece612fcc34c
Theo Portlock <zn.tportlock@gmail.com>
2023-02-13 09:39:36 +0100

    Fixed downsizing

--- bin/olddownstream.r
+++ bin/olddownstream.r
@@ -1,10 +1,4 @@
 #!/usr/bin/Rscript
-#SBATCH --account=snic2022-5-334
-#SBATCH --partition=node
-#SBATCH --ntasks=1
-#SBATCH --time=30:00:00
-#SBATCH --job-name=downmomr
-
 #memory.limit(9999999999)
 require('dplyr')  
 require('momr')
@@ -13,15 +7,22 @@ args = commandArgs(trailingOnly=TRUE)
 gctFile = args[1]
 indexedCatalog = args[2]
 mspdownload = args[3]
+#name = args[4]
 
 ## for testing only
-#gctFile = "tempgct.tsv"
+##gctFile = "/proj/uppstore2019028/projects/metagenome/theo/newscripts/neworalmerged/Downstream/gct.tsv"
+#gctFile = '/proj/uppstore2019028/nobackup/personal/theo/nmdsmeteor/gct.tsv'
 #mspdownload = "/proj/uppstore2019028/projects/metagenome/dataverse_files/IGC2.1990MSPs.tsv"
 #indexedCatalog = "/crex/proj/uppstore2019028/projects/metagenome/meteor_ref/oral_catalog/database/oral_catalog_lite_annotation"
+#gctFile = "/home/theop/downstream_data/norm.csv"
+#gctFile = "/crex/proj/snic2020-6-153/nobackup/private/gutnftest/work/6b/282d8e4afa311a83cdc89655f62e52/gct.tsv"
+#mspdownload = "/proj/uppstore2019028/projects/metagenome/dataverse_files/IGC2.1990MSPs.tsv"
 #indexedCatalog = "/crex/proj/uppstore2019028/projects/metagenome/meteor_ref/hs_10_4_igc2/database/hs_10_4_igc2_lite_annotation"
+#name = 'test'
 
 print("gct loading")
 gctTab = read.delim(gctFile, row.names=1, sep="\t", stringsAsFactors=F, header=T)
+#gctNorm10m = read.csv(gctFile, row.names=1, stringsAsFactors=F, header=T)
 print("gct loaded")
 
 print("gct info saving")
@@ -30,30 +31,31 @@ print(min(sampleSum))
 print(quantile(sampleSum,0.25))
 print(quantile(sampleSum,0.75))
 print(max(sampleSum))
+#write.csv(sampleSum, quote=F, file=gzfile(paste(outDir, "samplesum.csv.gz",sep='/')))
 write.csv(sampleSum, quote=F, file="samplesum.csv")
 rm(sampleSum)
 gc()
 print("gct info saved")
 
-print("downsizing begin")
-#need to include downsizing for GCT here also
-depth = 10000000
-gctdown10m = momr::downsizeMatrix(gctTab, level=depth, repetitions=1, silent=F)
-rm(gctTab)
-gc()
-print("downsizing finished")
+#print("downsizing begin")
+#depth = 10000000
+#gctdown10m = momr::downsizeMatrix(gctTab, level=depth, repetitions=1, silent=F)
+#rm(gctTab)
+#gc()
+#print("downsizing finished")
+gctdown10m <- gctTab
 
 print("norm begin")
 sizeTab = read.table(indexedCatalog, sep="\t", stringsAsFactors=F)
 names(sizeTab) <- c('gene_id', 'gene_size')
 genesizes = sizeTab$gene_size
 names(genesizes) = sizeTab$gene_id
-print('length of genesizes')
-print(length(genesizes))
-print('number of rows of gctdown10m')
-print(nrow(gctdown10m))
+#print('length of genesizes')
+#print(length(genesizes))
+#print('number of rows of gctdown10m')
+#print(nrow(gctdown10m))
 gctNorm10m = momr::normFreqRPKM(dat=gctdown10m, cat=genesizes)
-write.csv(gctNorm10m, quote=F, file="downNormGct.csv")
+#write.csv(gctNorm10m, quote=F, file="norm.csv")
 print("norm finished")
 
 print("catalog info loading")
@@ -83,5 +85,5 @@ length(genebag)
 mgs.dat <- momr::extractProfiles(mgs, data)
 mgs.med.vect <- momr::computeFilteredVectors(profile=mgs.dat, type="median")
 mgs.med.vect <- mgs.med.vect[rowSums(mgs.med.vect)>0,]
-write.csv(mgs.med.vect, quote=F, file="downsizedmsp.csv")
+write.csv(mgs.med.vect, quote=F, file="msp.csv")
 print("mgs generation done")
